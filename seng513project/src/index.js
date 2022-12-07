3bdc4e4fa2d90fdafe12e4532e824fd345454db4
MountainBikerYYC <ywang9696@gmail.com>
2022-12-07 00:12:18 -0700

    first commit of chatbox

--- seng513project/src/index.js
+++ seng513project/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
-
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+  
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+  
