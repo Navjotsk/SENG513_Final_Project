750fcd866a1ec37e712913786c3abbfd4aa02280
jocelyn-song <jia.song@ucalgary.ca>
2022-12-06 17:24:35 -0700

    main page

--- seng513project/src/components/Main.js
+++ seng513project/src/components/Main.js
@@ -1,7 +1,13 @@
 function Main() {
     return(
         <div className="container">
-            <h1>Hello World!</h1>
+            <div className="center">
+                <h1 className="title-text">PLAY MAD LIBS, WITH A FRIEND!</h1>
+                <div className="button-container">
+                    <div className="fat-button">PLAY A GUEST</div>
+                    <div className="fat-button">LOGIN NOW</div>
+                </div>
+            </div>
         </div>
     )
 }
