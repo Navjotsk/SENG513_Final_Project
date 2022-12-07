282c44365ad47c3905f6971fe2c91faaee26e279
MountainBikerYYC <ywang9696@gmail.com>
2022-12-07 00:21:36 -0700

    Moved chatbox into a component

--- seng513project/src/App.jsx
+++ seng513project/src/App.jsx
@@ -1,55 +1,11 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
-const socket = io.connect("http://localhost:5000");
+import './App.css';
+import ChatBox from './components/ChatBox';
 
 function App() {
-  //Joining a room
-  const [room, setRoom] = useState("");
-  const [isJoined, setIsJoined] = useState(false);
-
-  //Messages
-  const [message, setMessage] = useState("");
-  const [receivedMessage, setReceivedMessage] = useState("");
- 
-
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("joinRoom", room);
-    }
-    setIsJoined(true);
-  };
-
-  const sendMessage = () => {
-    socket.emit("sendMessage", { message, room });
-  };
-
-  useEffect(() => {
-    socket.on("receivedMessage", (data) => {
-      setReceivedMessage(data.message);
-    });
-  }, [socket]);
-
-
-  
   return (
     <div className="App">
-    <TextField id="outlined-basic" label="Outlined"  variant="outlined"  size="small" onChange={(event) => {
-          setRoom(event.target.value);
-        }}  />
-   
-    <Button variant="contained" onClick={joinRoom}>Join Room</Button>    
-      <TextField id="outlined-basic" label="Outlined" variant="outlined"  size="small" onChange={(event) => {
-          setMessage(event.target.value);
-        }}  />
-           
-    <Button variant="contained" onClick={sendMessage}>Send Message</Button>
-      {isJoined? <h1>Currently Playing in Room : {room}</h1>:<h1> Please Join a Room: </h1>}
-      {receivedMessage}
-     
+      <h1>Hello World!</h1>
+      <ChatBox/>
     </div>
   );
 }
