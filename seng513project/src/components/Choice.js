2a4e9eefc7943f0c414b67f4944b4278b713654f
jocelyn-song <jia.song@ucalgary.ca>
2022-12-08 13:47:11 -0700

    temp

--- seng513project/src/components/Choice.js
+++ seng513project/src/components/Choice.js
@@ -1,19 +1,19 @@
-function Choice() {
+function Choice({ handleSetLocation }) {
     return(
         <div className="container">
             <div className="center">
                 <h1 className="title-text">CHOOSE YOUR TOPIC:</h1>
                 <div className="button-container">
-                    <div className="fat-button">FOOD</div>
-                    <div className="fat-button">SPORT</div>
-                    <div className="fat-button">SCHOOL</div>
-                    <div className="fat-button">ANIME</div>
-                    <div className="fat-button">GAMES</div>
-                    <div className="fat-button">FANTASY</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>FOOD</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>SPORT</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>SCHOOL</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>ANIME</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>GAMES</div>
+                    <div className="fat-button" onClick={() => handleSetLocation('game')}>FANTASY</div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Choice
+export default Choice;
---
+++ seng513project/src/components/Game.js
@@ -0,0 +1,18 @@
+function Game() {
+    return(
+        <div className="container">
+            <div className="center">
+                <div className="button-container">
+                    <div className="fat-button">FOOD</div>
+                    <div className="fat-button">SPORT</div>
+                    <div className="fat-button">SCHOOL</div>
+                    <div className="fat-button">ANIME</div>
+                    <div className="fat-button">GAMES</div>
+                    <div className="fat-button">FANTASY</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Game;
