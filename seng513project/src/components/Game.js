43955e1d291c53b5a05d2c0d38db606cb99ff6e4
jocelyn-song <jia.song@ucalgary.ca>
2022-12-08 17:56:18 -0700

    blur

--- seng513project/src/components/Game.js
+++ seng513project/src/components/Game.js
@@ -1,15 +1,18 @@
 function Game() {
     return(
         <div className="container">
-            <div className="center">
-                <div className="button-container">
-                    <div className="fat-button">FOOD</div>
-                    <div className="fat-button">SPORT</div>
-                    <div className="fat-button">SCHOOL</div>
-                    <div className="fat-button">ANIME</div>
-                    <div className="fat-button">GAMES</div>
-                    <div className="fat-button">FANTASY</div>
+            <div className="game-container">
+                <h1 className="story-title">TACOS? TACOS!</h1>
+                <div className="text-container">
+                    <form action="">
+                        <span>Lorem ipsum dolor sit amet, <input type="text" placeholder="verb"/></span>
+                        <span> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br/>Lorem ipsum dolor sit amet, <input type="text" placeholder="verb"/></span>
+                        <span> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br/>Lorem ipsum dolor sit amet, <input type="text" placeholder="verb"/></span>
+                        <span> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br/>Lorem ipsum dolor sit amet, <input type="text" placeholder="verb"/></span>
+                        <span> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+                    </form>
                 </div>
+                <div className="small-button">FINISH</div>
             </div>
         </div>
     )
