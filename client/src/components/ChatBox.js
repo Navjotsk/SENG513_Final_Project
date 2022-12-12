06040637dc0294cba2e6be0e2a223bd1705751ac
MountainBikerYYC <ywang9696@gmail.com>
2022-12-12 13:42:52 -0700

    Chatbox works with multiple clients

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -20,17 +20,7 @@ function ChatBox({ handleSetInput, handleSendMessage, messages }) {
         }}  />
             
       <Button variant="contained" onClick={handleSendMessage}>Send Message</Button>
-        {/* {messages.map((object, i) => {
-          return (
-            object.me ? (
-              <div key={"message"+i} className='my-message'>{object.text}</div>
-            ) : (
-              <div key={"message"+i}className='other-message'>{object.text}</div>
-            )
-          )
-        })} */}
-     
-    </div>
+        </div>
     </div>
   );
 }
