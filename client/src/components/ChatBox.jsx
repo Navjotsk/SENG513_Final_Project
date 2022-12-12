907f32de352c86b9935f528695e0d7c2549c45af
jocelyn-song <jia.song@ucalgary.ca>
2022-12-11 22:07:47 -0700

    fixed chat in room

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,53 +1,24 @@
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-const socket = io.connect("http://localhost:5000");
-
-function ChatBox() {
-  //Joining a room
-  const [room, setRoom] = useState("");
-  const [isJoined, setIsJoined] = useState(false);
-
-  //Messages
-  const [message, setMessage] = useState("");
-  const [receivedMessage, setReceivedMessage] = useState("");
- 
-
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("joinRoom", room);
-    }
-    setIsJoined(true);
-  };
-
-  const sendMessage = () => {
-    socket.emit("sendMessage", { message, room });
-  };
-
-  useEffect(() => {
-    socket.on("receivedMessage", (data) => {
-      setReceivedMessage(data.message);
-    });
-  }, [socket]);
-
-
-  
-  return (
-    <div className="App">
-    <TextField id="outlined-basic" label="Outlined"  variant="outlined"  size="small" onChange={(event) => {
-          setRoom(event.target.value);
-        }}  />
+function ChatBox({ handleSetInput, handleSendMessage, messages }) {
    
-    <Button variant="contained" onClick={joinRoom}>Join Room</Button>    
-      <TextField id="outlined-basic" label="Outlined" variant="outlined"  size="small" onChange={(event) => {
-          setMessage(event.target.value);
+  return (
+    <div className="App"> 
+      <TextField id="text-field" label="Outlined" variant="outlined"  size="small" onChange={(event) => {
+          handleSetInput(event.target.value);
         }}  />
-           
-    <Button variant="contained" onClick={sendMessage}>Send Message</Button>
-      {isJoined? <h1>Currently Playing in Room : {room}</h1>:<h1> Please Join a Room: </h1>}
-      {receivedMessage}
+            
+      <Button variant="contained" onClick={handleSendMessage}>Send Message</Button>
+        {messages.map((object, i) => {
+          return (
+            object.me ? (
+              <div key={"message"+i} className='my-message'>{object.text}</div>
+            ) : (
+              <div key={"message"+i}className='other-message'>{object.text}</div>
+            )
+          )
+        })}
      
     </div>
   );
