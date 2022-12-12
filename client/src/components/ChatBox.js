480ab3456de83f5102d4d187d04f7dca73c599ba
MountainBikerYYC <ywang9696@gmail.com>
2022-12-12 13:42:01 -0700

    Moved chatbox to bottom right, working

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -4,13 +4,8 @@ import Button from '@mui/material/Button';
 function ChatBox({ handleSetInput, handleSendMessage, messages }) {
    
   return (
-    <div className="App"> 
-      <TextField id="text-field" label="Outlined" variant="outlined"  size="small" onChange={(event) => {
-          handleSetInput(event.target.value);
-        }}  />
-            
-      <Button variant="contained" onClick={handleSendMessage}>Send Message</Button>
-        {messages.map((object, i) => {
+    <div className="chatBox"> 
+    {messages.map((object, i) => {
           return (
             object.me ? (
               <div key={"message"+i} className='my-message'>{object.text}</div>
@@ -19,7 +14,23 @@ function ChatBox({ handleSetInput, handleSendMessage, messages }) {
             )
           )
         })}
+    <div className="messageBox">
+      <TextField id="text-field" label="Outlined" variant="outlined"  size="small" onChange={(event) => {
+          handleSetInput(event.target.value);
+        }}  />
+            
+      <Button variant="contained" onClick={handleSendMessage}>Send Message</Button>
+        {/* {messages.map((object, i) => {
+          return (
+            object.me ? (
+              <div key={"message"+i} className='my-message'>{object.text}</div>
+            ) : (
+              <div key={"message"+i}className='other-message'>{object.text}</div>
+            )
+          )
+        })} */}
      
+    </div>
     </div>
   );
 }
